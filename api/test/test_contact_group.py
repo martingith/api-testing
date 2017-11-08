daa7fba58e6ae7fb9f8a7e533e1df2cb45fa8f76
Karl <riisikarl@gmail.com>
2017-11-08 16:42:49 +0200

    added the last test and commented a bit

--- api/test/test_contact_group.py
+++ api/test/test_contact_group.py
@@ -25,39 +25,6 @@ def test_add_contact_group_with_only_name():
     # Assert that group list contains the group that was added by the test
     assert count_contact_groups_by_name(my_group_name) == 1
 
-def test_add_contact_group_with_invalid_values():
-
-    r1 = add_contact_group(group_name="", group_type="person", description="test description")
-    assert r1.status_code == 400
-
-def test_remove_organization_from_contact_group():
-    # creates a new group, adds a organization to there and then removes the organization from the group
-
-
-    organization = add_organization("Test organization" + get_time_in_milliseconds())
-    organization_id = organization.json['data']['id']
-
-    contact_group = add_contact_group(group_name="group" + get_time_in_milliseconds(), group_type="organization",
-                                      description="Test group description")
-    contact_group_id = contact_group.json['data']['id']
-
-    endpoint = 'contactGroups/' + str(contact_group_id) + '/organizations'
-    payload = {
-        "item_ids": [organization_id]
-    }
-
-    r1 = do_request(method='POST', endpoint=endpoint, data=payload)
-    # removes the organization from the group
-    r2 = do_request(method='DELETE', endpoint=endpoint, data=payload)
-
-    assert r1.status_code == 200
-    assert r1.json['success']
-    # Assert response code
-    assert r2.status_code == 200
-    # Assert that in API response success==true
-    assert r2.json['success']
-
-
 def test_add_person_to_contact_group():
     person = add_person("Test person" + get_time_in_milliseconds())
     person_id = person.json['data']['id']
@@ -77,6 +44,53 @@ def test_add_person_to_contact_group():
     # Assert that in API response success==true
     assert r.json['success']
 
+def test_add_organization_to_contact_group():
+    organization = add_organization("Test organization" + get_time_in_milliseconds())
+    organization_id = organization.json['data']['id']
+
+    contact_group = add_contact_group(group_name="group" + get_time_in_milliseconds(), group_type="organization",
+                                      description="Test group description")
+    contact_group_id = contact_group.json['data']['id']
+
+    endpoint = 'contactGroups/' + str(contact_group_id) + '/organizations'
+    payload = {
+        "item_ids": [organization_id]
+    }
+    r = do_request(method='POST', endpoint=endpoint, data=payload)
+
+    # Assert response code
+    assert r.status_code == 200
+    # Assert that in API response success==true
+    assert r.json['success']
+
+
+
+def test_remove_organization_from_contact_group():
+    # creates a new group, adds a organization there and then removes the organization from the group
+
+    organization = add_organization("Test organization" + get_time_in_milliseconds())
+    organization_id = organization.json['data']['id']
+
+    contact_group = add_contact_group(group_name="group" + get_time_in_milliseconds(), group_type="organization",
+                                      description="Test group description")
+    contact_group_id = contact_group.json['data']['id']
+
+    endpoint = 'contactGroups/' + str(contact_group_id) + '/organizations'
+    payload = {
+        "item_ids": [organization_id]
+    }
+
+    r1 = do_request(method='POST', endpoint=endpoint, data=payload)
+    # removes the organization from the group
+    r2 = do_request(method='DELETE', endpoint=endpoint, data=payload)
+
+    # Assert response code
+    assert r1.status_code == 200
+    # Assert that in API response success==true
+    assert r1.json['success']
+
+    assert r2.status_code == 200
+    assert r2.json['success']
 
 def test_rename_group():
     # adds a group and then renames it
@@ -97,8 +111,15 @@ def test_rename_group():
     # Assert that in API response success==true
     assert r.json['success']
 
+def test_add_contact_group_with_invalid_values():
+    # tries adding a group with a blank name
+
+    r1 = add_contact_group(group_name="", group_type="person", description="test description")
+    assert r1.status_code == 400
 
 def test_person_in_deleted_group_sent_to_ungrouped_contacts():
+    # checks if a group is deleted, then the person who was in that group is
+    # moved to the ungrouped contacts group
 
     # creates a new person
     person = add_person("Test person" + get_time_in_milliseconds())
@@ -125,7 +146,8 @@ def test_person_in_deleted_group_sent_to_ungrouped_contacts():
     payload = {
         "item_ids": [contact_group_id]
     }
-    # removes the contact group
+
+    # deletes the contact group
     r2 = do_request(method='DELETE', endpoint=endpoint, data=payload)
     assert r2.status_code == 200
     assert r2.json['success']
