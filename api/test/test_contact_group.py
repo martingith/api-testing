789d15d64b5f3c1494ef62c1d1940ae2b9e6bd0d
liinaanette <liinaanette@gmail.com>
2017-11-08 12:23:20 +0200

    4 out of five done

--- api/test/test_contact_group.py
+++ api/test/test_contact_group.py
@@ -1,5 +1,4 @@
-from api.pipedrive_utils import add_contact_group, get_contact_groups, count_contact_groups_by_name
-from api.pipedrive_utils import add_person
+from api.pipedrive_utils import *
 from api.utils import do_request, get_time_in_milliseconds
 
 
@@ -13,7 +12,7 @@ def teardown():
     print("teardown")
 
 
-def test_add_contact_group_with_only_name():
+"""def test_add_contact_group_with_only_name():
     my_group_name = "group" + get_time_in_milliseconds()
     r = add_contact_group(group_name=my_group_name, group_type="person",
                           description="Test group description")
@@ -44,3 +43,82 @@ def test_add_person_to_contact_group():
     assert r.status_code == 200
     # Assert that in API response success==true
     assert r.json['success']
+
+def test_add_organization_to_group():
+    group = add_contact_group("group1","organization","This is automatically generated")
+    group_id = group.json['data']['id']
+    organization = add_organization("org1")
+    organization_id = organization.json['data']['id']
+
+    endpoint = 'contactGroups/' + str(group_id) + '/organizations'
+    payload = {
+        "item_ids": [organization_id]
+    }
+    r = do_request(method='POST', endpoint=endpoint,data=payload)
+
+    assert r.status_code == 200
+    assert r.json['success'] 
+
+def test_removing_org_from_contact_group():
+    group = add_contact_group("group5", "organization", "This is automatically generated")
+    group_id = group.json['data']['id']
+    organization = add_organization("org1")
+    organization_id = organization.json['data']['id']
+    organization2 = add_organization("org2")
+    organization2_id = organization2.json['data']['id']
+
+    endpoint = 'contactGroups/' + str(group_id) + '/organizations'
+    payload = {
+        "item_ids": [organization_id,organization2_id]
+    }
+    r = do_request(method='POST', endpoint=endpoint, data=payload)
+    assert r.status_code == 200
+
+    k = do_request(method='DELETE',endpoint=endpoint+'/',data=payload)
+    assert k.status_code == 200
+    assert k.json['success'] 
+
+def test_rename_a_group():
+    group = add_contact_group("group5", "organization", "This is automatically generated")
+    group_id = group.json['data']['id']
+    endpoint = 'contactGroups/' + str(group_id)
+    payload = {
+        "name": "uus grupi nimi",
+    }
+    r = do_request('PUT',endpoint=endpoint,data=payload)
+    assert r.status_code == 200
+    assert r.json['success'] 
+
+def test_add_contact_group_with_invalid_values():
+    group = add_contact_group("","person","description")
+
+    assert group.status_code == 400
+    assert not group.json['success'] """
+
+def test_after_deleting_group_does_organization_move_to_ungrouped():
+    group = add_contact_group("group6", "organization", "This is automatically generated for deleting")
+    group_id = group.json['data']['id']
+    organization = add_organization("org1")
+    organization_id = organization.json['data']['id']
+
+    endpoint = 'contactGroups/' + str(group_id) + '/organizations'
+    payload = {
+        "item_ids": [organization_id]
+    }
+    r = do_request(method='POST', endpoint=endpoint, data=payload)
+    k = do_request('GET',endpoint='contactGroups/1')
+
+    algne = int(k.json['additional_data']['group_summary']['organization']['items_total'])
+
+    payload2 = {
+        "id":str(group_id)
+    }
+
+    o = do_request('DELETE',endpoint='contactGroups/',data=payload2)
+    assert o.status_code == 200
+    assert o.json['success']
+
+    k = do_request('GET',endpoint='contactGroups/1')
+    pärast = int(k.json['additional_data']['group_summary']['organization']['items_total'])
+
+    assert pärast -1 == algne
